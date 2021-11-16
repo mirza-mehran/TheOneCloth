641cec5242e2542fb83de38b44592440ddca3fdb
mirza-mehran <mirzamehran1134@gmail.com>
2021-11-17 02:05:09 +0500

    Ready Project Structure For Development

---
+++ Solution1/TheOneCloth.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheOneCloth.Web.MvcApplication" Language="C#" %>
---
+++ Solution1/TheOneCloth.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheOneCloth.Web.Startup))]
+namespace TheOneCloth.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
