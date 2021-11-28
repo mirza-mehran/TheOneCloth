2ab4d9a3051c41b9b9c9129012058fbb65efa8f2
mirza-mehran <mirzamehran1134@gmail.com>
2021-11-29 01:25:05 +0500

    Apply Traditional/ Conventional Routing in Admin Area Categories

--- Solution1/TheOneCloth.Web/Areas/Admin/AdminAreaRegistration.cs
+++ Solution1/TheOneCloth.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -12,13 +12,22 @@ namespace TheOneCloth.Web.Areas.Admin
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
+            context.MapRoute(
+                name: "allcategory",
+                url: "categories",
+                defaults: new { controller = "Main_Category", action = "Index", id = UrlParameter.Optional }
+            );
+
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
             );
+
+          
+
         }
     }
 }
